cf89d0605bb8680475ef90fadc7a20ccc07f756e
Ravi-Coder <156787721+Ravi-kant-coder@users.noreply.github.com>
2024-01-24 16:36:42 +0530

    First experiment with next js

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+import styles from "@/styles/Home.module.css";
+
+export function Footer() {
+  return (
+    <>
+      <h1>This is the Footer Copyright@Nihongomax</h1>
+      <a target="_blank" href="https://www.nihongomax.com">
+        nihongomax.com
+      </a>
+    </>
+  );
+}
